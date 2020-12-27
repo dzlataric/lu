d58e99a5f508ccac9c5f1b581540c570ac2ec457
Dejan Zlataric <d.zlataric@levi9.com>
2020-12-27 23:33:52 +0100

    Initialized project

---
+++ src/main/java/rs/ns/lu/LuApplication.java
@@ -0,0 +1,13 @@
+package rs.ns.lu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuApplication.class, args);
+	}
+
+}
